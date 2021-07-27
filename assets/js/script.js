a18c5bb1d7340b323dbf8f518442e5a0abaa3d4e
Alena Rehberger <alenarehberger92@gmail.com>
2021-07-26 20:34:55 -0500

    Got started on basic html and linked bulma to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,31 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+var cityList = document.querySelector('#previous-search');
+var fetchButton = document.getElementById('fetch-button');
+
+function getApi() {
+  var requestUrl = 'https://openweathermap.org/api.openweathermap.org/data/2.5/weather?q=London&appid=fa591f1f8cf83f15d34fef752ceddbfa';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+    //   for (var i = 0; i < data.length; i++) { 
+    //   var listItem = document.createElement('li');
+    //   listItem.textContent = data[i].weather[0].icon
+        console.log(data);
+    });
+    
+fetchButton.addEventListener('click', getApi);
+
+// // jQuery API
+// $.ajax({
+//     url: 'https://openweathermap.org/api.openweathermap.org/data/2.5/weather?q=Baxter&appid=fa591f1f8cf83f15d34fef752ceddbfa'
+//     method: "GET"
+// }).then (function (response) {
+//     console.log (response)
+
+// })
